ddc68b82a4c0f7985fef5cab10c4948e6c648cd2
Alex Nazarov <alexnazarov121@yandex.ru>
2025-09-06 19:39:01 +0300

    create new test for negative checks

---
+++ test_negative_checks.py
@@ -0,0 +1,24 @@
+from .pages.product_page import ProductPage
+import pytest
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_disappeared_success_message()
