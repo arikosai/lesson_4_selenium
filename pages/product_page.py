9dd4bf6b24baa17b13839d11f8bd161a7b8ed8f2
Alex Nazarov <alexnazarov121@yandex.ru>
2025-09-06 19:38:25 +0300

    add 2 new test methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,9 +9,17 @@ class ProductPage(BasePage):
     def should_be_product_in_basket(self):
         name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         name_alert = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ALERT)
-        assert name.text == name_alert.text, 'product is not in basket'
+        assert name.text == name_alert.text, 'Product is not in basket'
 
     def should_be_basket_price(self):
         price = self.browser.find_element(*ProductPageLocators.PRICE)
         price_alert = self.browser.find_element(*ProductPageLocators.PRICE_ALERT)
-        assert price.text == price_alert.text, 'prices are not the same'
+        assert price.text == price_alert.text, 'Prices are not the same'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+        
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message did not disappear, but should be"
