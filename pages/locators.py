56e06e17c5396a22bc671163a53ab3341786ef02
Alex Nazarov <alexnazarov121@yandex.ru>
2025-09-07 14:29:04 +0300

    add new locator for BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")    
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')    
     BASKET_LINK = (By.CSS_SELECTOR, '.btn-group a.btn.btn-default')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET = (By.CSS_SELECTOR, '#content_inner')
@@ -14,8 +15,13 @@ class MainPageLocators():
     pass
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_PASSWORD_CONFIRM = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '.register_form .btn')
+
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
