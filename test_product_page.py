1317153bfbc67b4ad0fbf3fb4297bb915472c934
Alex Nazarov <alexnazarov121@yandex.ru>
2025-09-07 14:27:23 +0300

    add test class with new tests

--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,34 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
+import time
 
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+@pytest.mark.new
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link_log = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        email = str(time.time()) + "@fakemail.org"
+        login_page = LoginPage(browser, link_log)
+        login_page.open()
+        login_page.register_new_user(email, '1afsdf=23456789aA')
+        login_page.should_be_authorized_user()
+    
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_product_in_basket()
+        page.should_be_basket_price()
+
 
 @pytest.mark.parametrize('num', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, num):
